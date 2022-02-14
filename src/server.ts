cc24f79f70ef067bdaa544f7e05a2c8394c09845
Daniel Facciolo Pires <Daniel.Pires@fatec-srv.local>
2022-02-14 08:49:59 -0300

    typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log(`Funcionou nosso projeto Node com TS`)
+import express from 'express'
+
+let server = express()
+
+server.use(express.json())
+
+server.listen(3333, () => {
+    console.log('Servidor iniciado ')
+})
